ebb6186a97595c134ef285c74571c8c9279dab59
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2020-12-11 18:09:23 +0100

    Define BTree model and draft find_height method

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ class Node:
     self.left = None
     self.right = None
 
+# Binary Tree
+class BTree:
+  def __init__(self, root):
+    self.root = root
+
+  # Find binary tree depth (recursive)
+  def find_height(self, node):
+
+    if(node is None):
+      return 0
+
+    lheight = self.find_height(node.left) 
+    rheight = self.find_height(node.right)
+
+    if(lheight > rheight):
+      height = 1 + lheight
+    else:
+      height = 1 + rheight
+      
+    return height
+
+  def height(self):
+    return self.find_height(self.root)
+
 # Main
 def main():
   root = Node(0)
-  print(root.value)
+  tree = BTree(root)
+  tree.root.right = Node(1)
+  print(tree.height())
 
 if __name__ == "__main__":
   main()
